700073be25e6913e4f0e839dac6b267ed03e086e
Supriya-sia461 <75336244+Supriya-sia461@users.noreply.github.com>
2021-03-19 17:02:30 -0500

    Add files via upload

---
+++ Crud_Project/Crud_Project/crudapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from crudapp.models import log
+
+admin.site.register(log)
---
+++ Crud_Project/Crud_Project/crudapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from django.urls import reverse
+
+class log(models.Model):
+    name = models.CharField(max_length=200)
+    pages = models.IntegerField()
+
+    def __str__(self):
+        return self.name
